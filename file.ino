0aee71c17c1671fb4f8350dd9c0fc80a1a3cafcc
dhvfr <win10pro.2k4lite@outlook.com>
2025-06-11 16:46:50 +0700

    Firmware

--- file.ino
+++ file.ino
@@ -1,19 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-const char *ssid = "Remote GPIO 2";
+const char *ssid = "System";
 const char *password = "12345678";
-const int outputPin = D4;
 ESP8266WebServer server(80);
-const char systempage[] PROGMEM = R"rawliteral(
+const char html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>
-System
-</title>
+<title>System</title>
 <style>
 *{
 margin: 0;
@@ -28,7 +25,7 @@ height: 100vh;
 overflow: hidden;
 box-sizing: border-box;
 font-family: Consolas, monaco, monospace;
-background-color: #1c1c1d;
+background-color: #1f1f1f;
 background-repeat: no-repeat;
 background-size: cover;
 background-position: center;
@@ -42,7 +39,7 @@ user-select: none;
 position: absolute;
 padding: 25px;
 width: 400px;
-background-color: #252728;
+background-color: #2b2b2b;
 }
 .prompt p {
 color: #32cd32;
@@ -56,86 +53,77 @@ font-family: Consolas, monaco, monospace;
 font-size: 14px;
 font-weight: 500;
 margin: 10px 0;
-background-color: #3b3d3e;
-color: #32cd32;
+background-color: #3a3a3a;
+color: #ffffff
 }
 .prompt button:hover {
-background-color: #1c1c1d;
+background-color: #252728;
 }
 .prompt button:active {
-background-color: #000000;
+background-color: #1f1f1f;
 }
 </style>
 <body>
 <div class="prompt">
-<button id="outputbutton" ontouchstart="toggleLED('on')" ontouchend="toggleLED('off')" onmousedown="toggleLED('on')" onmouseup="toggleLED('off')">
-Signal LED light and output GPIO2 (D4)
+<button onclick="outerpage()">
+Required ethernet or mobile data
 </button>
-<button onclick="location.reload();">
-Update memory status now (Don't turn off system or change network)
-</button>
-<p>
-Free heap: %FREE_HEAP% bytes
+<p id="freeheap">
+Free Heap: ...
 </p>
-<p>
-Heap fragmentation: %HEAP_FRAG%%
+<p id="maxblock">
+Max Free Block Size: ...
 </p>
-<p>
-Max free block size: %MAX_BLOCK% bytes
+<p id="frag">
+Heap Fragmentation: ...
 </p>
 </div>
 </body>
 <script>
-function toggleLED(state) {
-fetch("/led?state=" + state);
+function outerpage() {
+window.open("https://www.facebook.com")
 }
+function fetchmemory() {
+fetch('/memory')
+.then(response => response.json())
+.then(data => {
+document.getElementById("freeheap").innerText = "Free Heap: " + data.freeheap + " byte";
+document.getElementById("maxblock").innerText = "Max Free Block Size: " + data.maxblock + " bytes"
+document.getElementById("frag").innerText = "Heap Fragmentation: " + data.frag + " %"
+})
+}
+setInterval(fetchMemory, 1000)
+fetchMemory()
 </script>
 </head>
 </html>
 )rawliteral";
-String processor(const String& var) {
-if (var == "FREE_HEAP") return String(ESP.getFreeHeap());
-if (var == "HEAP_FRAG") return String(ESP.getHeapFragmentation());
-if (var == "MAX_BLOCK") return String(ESP.getMaxFreeBlockSize());
-return String();
-}
 void handleRoot() {
-String html = systempage;
-html.replace("%FREE_HEAP%", processor("FREE_HEAP"));
-html.replace("%HEAP_FRAG%", processor("HEAP_FRAG"));
-html.replace("%MAX_BLOCK%", processor("MAX_BLOCK"));
 server.send(200, "text/html", html);
 }
-void handleLED() {
-String state = server.arg("state");
-if (state == "on") {
-digitalWrite(outputPin, LOW);
-}
-else {
-digitalWrite(outputPin, HIGH);
-}
-server.send(200, "text/plain", "Button command");
-}
-void handleOff() {
-server.send(200, "text/plain", "System is shuting down...");
-delay(1000);
-ESP.deepSleep(0);
+void handleMemory() {
+String json = "{";
+json += "\"freeheap\":" + String(ESP.getFreeHeap()) + ",";
+json += "\"maxblock\":" + String(ESP.getMaxFreeBlockSize()) + ",";
+json += "\"frag\":" + String(ESP.getHeapFragmentation());
+json += "}";
+server.send(200, "application/json", json);
 }
 void setup() {
 Serial.begin(115200);
-pinMode(outputPin, OUTPUT);
-digitalWrite(outputPin, HIGH);
 WiFi.softAP(ssid, password);
-while (WiFi.status() != WL_CONNECTED) delay(500);
 Serial.println("Access Point Started");
 Serial.print("IP address: ");
-Serial.println(WiFi.softAPIP());
+Serial.println(WiFi.softAPIP().toString());
 server.on("/", handleRoot);
-server.on("/led", handleLED);
-server.on("/shutdown", handleOff);
+server.on("/memory", handleMemory);
 server.begin();
 Serial.println("Web server started");
 }
 void loop() {
+digitalWrite(LED_BUILTIN, LOW);
+delay(125);
+digitalWrite(LED_BUILTIN, HIGH);
+delay(125);
 server.handleClient();
 }
